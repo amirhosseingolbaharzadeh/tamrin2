f69cd1af3390380368d0006e473ab56121b24ec7
amirhosseingolbaharzadeh <159616881+amirhosseingolbaharzadeh@users.noreply.github.com>
2024-02-11 21:25:10 +0330

    Update main (1).c

--- main (1).c
+++ main (1).c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
   // An array of ASCII values that represent the name
-  int name[] = {97, 109, 105, 114, 104, 111, 115, 115, 101, 114, 105, 110, 32, 103, 111, 108, 98, 97, 104, 97, 114, 122, 97, 100, 101, 104};
+  int name[] = {97, 109, 105, 114, 104, 111, 115, 115, 101, 105, 110, 32, 103, 111, 108, 98, 97, 104, 97, 114, 122, 97, 100, 101, 104};
   int i;
   // Loop through the array and print each character
   for (i = 0; i < 26; i++) {
